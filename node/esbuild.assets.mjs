91cdd6a074d20c5ca276a404933940ef122f6613
Thomas van der Velde <contact@toolbarthomas.net>
2023-08-03 20:27:16 +0200

    update: Include global stylesheet and initial config

---
+++ node/esbuild.assets.mjs
@@ -0,0 +1,32 @@
+import esbuild from "esbuild";
+import { stylePlugin } from "@toolbarthomas/enlightenment/node/esbuild.style.plugin.mjs";
+import { resolvePlugin } from "@toolbarthomas/enlightenment/node/esbuild.resolve.plugin.mjs";
+import { argv } from "@toolbarthomas/enlightenment/node/argv.mjs";
+
+(async () => {
+  const suffix = argv.m || argv.minify ? ".min" : "";
+  const outdir = "dist";
+  const outExtension = {
+    ".js": `${suffix}.js`,
+  };
+
+  const config = {
+    bundle: true,
+    entryPoints: ["src/index.ts"],
+    format: "esm",
+    keepNames: true,
+    minify: argv.m || argv.minify || false,
+    outdir,
+    outExtension,
+    platform: "browser",
+    plugins: [stylePlugin()],
+  };
+
+  if (argv.d || argv.devmode) {
+    (await esbuild.context(config)).watch();
+  } else {
+    esbuild.build(config).then(() => {
+      console.log(`Enlightenment assets generated: ${outdir}/*${suffix}.js`);
+    });
+  }
+})();
---
+++ src/index.ts
@@ -0,0 +1,9 @@
+/**
+ * UI entry file that should define the global assets like webfonts,
+ * CSS variables and many more.
+ */
+
+import styles from "./styles/index.scss";
+
+// Call the definition once to ensure it is picked up by the compiler.
+(() => styles)();
