044d4dc27e2a03f970de07e8649ea05ea292968c
Thomas van der Velde <contact@toolbarthomas.net>
2023-08-03 20:39:05 +0200

    chore: Update global asset documentation

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,14 @@
 /**
  * UI entry file that should define the global assets like webfonts,
  * CSS variables and many more.
+ *
+ * No logic should be defined within this file since it is only a reference for
+ * the global assets for the generated package.
+ *
+ * The imported assets should be available within the global scope. This means
+ * that the actual assets should be available within the Enlightenment component
+ * context (mostly css variables).
  */
-
 import styles from "./styles/index.scss";
 
 // Call the definition once to ensure it is picked up by the compiler.
