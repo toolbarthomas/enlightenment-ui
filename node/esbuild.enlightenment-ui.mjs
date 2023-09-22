25b3df1616003997aa5b36f252f53888484b7b9a
Thomas van der Velde <contact@toolbarthomas.net>
2023-09-22 18:28:07 +0200

    update: Split components & plugins, includes FocusTrap

--- node/esbuild.enlightenment-ui.mjs
+++ node/esbuild.enlightenment-ui.mjs
@@ -1,49 +1,46 @@
-import esbuild from "esbuild";
-import { stylePlugin } from "@toolbarthomas/enlightenment/node/esbuild.style.plugin.mjs";
-import { resolvePlugin } from "@toolbarthomas/enlightenment/node/esbuild.resolve.plugin.mjs";
-import { argv } from "@toolbarthomas/enlightenment/node/argv.mjs";
-import { globSync } from "glob";
+import esbuild from 'esbuild'
+import { stylePlugin } from '@toolbarthomas/enlightenment/node/esbuild.style.plugin.mjs'
+import { resolvePlugin } from '@toolbarthomas/enlightenment/node/esbuild.resolve.plugin.mjs'
+import { argv } from '@toolbarthomas/enlightenment/node/argv.mjs'
+import { globSync } from 'glob'
 
-import { Enlightenment } from "@toolbarthomas/enlightenment/index.mjs";
+import { Enlightenment } from '@toolbarthomas/enlightenment/index.mjs'
 
-import config from "./config.mjs";
-import { svgspritePlugin } from "./esbuild.svgsprite.plugin.mjs";
+import config from './config.mjs'
+import { svgspritePlugin } from './esbuild.svgsprite.plugin.mjs'
 
-import { defaultLoader } from "./utils/loader.mjs";
-
-(async () => {
+import { defaultLoader } from './utils/loader.mjs'
+;(async () => {
   const options = {
     bundle: true,
-    entryPoints: [...globSync("./src/components/*.ts")],
-    format: argv.format || "esm",
+    entryPoints: [...globSync('./src/components/*.ts'), ...globSync('./src/plugins/*.ts')],
+    format: argv.format || 'esm',
     keepNames: true,
     loader: { ...defaultLoader },
     minify: argv.m || argv.minify || false,
     outdir: config.outdir,
     outExtension: config.outExtension,
-    platform: argv.platform || "browser",
+    platform: argv.platform || 'browser',
     plugins: [
       resolvePlugin({
-        destination: `./${argv.name || "enlightenment"}${config.suffix}.js`,
-        minify: argv.m || argv.minify || false,
+        destination: `../${argv.name || 'enlightenment'}${config.suffix}.js`,
+        minify: argv.m || argv.minify || false
       }),
       stylePlugin(),
-      svgspritePlugin(),
-    ],
-  };
+      svgspritePlugin()
+    ]
+  }
 
   if (argv.d || argv.devmode) {
-    const context = await esbuild.context(options);
+    const context = await esbuild.context(options)
     context.serve({ servedir: options.outdir }).then((result) => {
       console.log(
         `Enlightenment UI test server starter: ${result.host}:${result.port} => ${config.outdir}`
-      );
-    });
+      )
+    })
   } else {
     esbuild.build(options).then(() => {
-      console.log(
-        `Enlightenment UI library created: ${options.outdir}/*${config.suffix}.js`
-      );
-    });
+      console.log(`Enlightenment UI library created: ${options.outdir}/*${config.suffix}.js`)
+    })
   }
-})();
+})()
