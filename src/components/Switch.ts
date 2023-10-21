519c5f8e664bc4ceffa081fe85d6fe7e0a15dbc1
Thomas van der Velde <contact@toolbarthomas.net>
2023-10-21 22:39:05 +0200

    update: Initial form switch component

---
+++ src/components/Switch.ts
@@ -0,0 +1,120 @@
+import {
+  createRef,
+  customElement,
+  Enlightenment,
+  html,
+  nothing,
+  property,
+  ref
+} from '@toolbarthomas/enlightenment'
+
+import styles from '@/components/Switch.scss'
+
+@customElement('ui-switch')
+class EnlightenmentRadioButton extends Enlightenment {
+  static styles = [styles]
+
+  @property({ attribute: 'active', converter: Enlightenment.isBoolean, type: Boolean })
+  isActive?: boolean = false
+
+  @property({ type: String })
+  label?: string = ''
+
+  @property({ type: String })
+  name?: string
+
+  @property({ type: String })
+  value?: string
+
+  @property({ converter: Enlightenment.isBoolean, type: Boolean })
+  disabled?: boolean
+
+  handleChange(event: Event) {
+    if (!event || !event.target) {
+      return
+    }
+
+    this.commit('isActive', event.target.checked)
+  }
+
+  handleClick(event: Event) {
+    const target = event.target as HTMLElement
+
+    if (target.tagName === 'INPUT') {
+      event.preventDefault()
+
+      return
+    }
+
+    if (this.disabled) {
+      return
+    }
+
+    const input = this.useContext() as HTMLInputElement
+
+    if (!input) {
+      return
+    }
+
+    input.checked = !this.isActive
+
+    this.hook('change', { context: input })
+  }
+
+  handleKeydown(event: KeyboardEvent) {
+    const { keyCode } = event
+
+    if (Enlightenment.keyCodes.meta.includes(keyCode)) {
+      return
+    }
+
+    if (Enlightenment.keyCodes.exit.includes(keyCode)) {
+      event.target && event.target.blur()
+
+      return
+    }
+
+    if (!Enlightenment.keyCodes.confirm.includes(keyCode)) {
+      return
+    }
+
+    this.handleClick({ target: this })
+  }
+
+  render() {
+    const classes = []
+
+    if (this.disabled) {
+      classes.push('switch--is-disabled')
+    }
+
+    if (this.isActive) {
+      classes.push('switch--is-active')
+    }
+
+    return html`
+      <div class="switch ${classes.join(' ')}" @click=${this.handleClick}>
+        <label class="switch__label">
+          <span class="switch__input-wrapper">
+            <input
+              ?checked=${this.isActive && 'checked'}
+              ?disabled=${this.disabled}
+              ?name=${this.name}
+              ?value=${this.value}
+              @change=${this.handleChange}
+              @click=${this.handleClick}
+              @keydown=${this.handleKeydown}
+              class="switch__input"
+              ref="{${ref(this.context)}}"
+              type="checkbox"
+            />
+            <span class="switch__icon"></span>
+          </span>
+          <span class="switch__body">
+            <slot>${this.label || nothing}</slot>
+          </span>
+        </label>
+      </div>
+    `
+  }
+}
