ff89d96632b8c5e2183c4a183f3873e22963783e
Thomas van der Velde <contact@toolbarthomas.net>
2023-11-28 00:15:09 +0100

    update: Initial window types

--- src/components/Window.ts
+++ src/components/Window.ts
@@ -3,6 +3,7 @@ import {
   customElement,
   Enlightenment,
   html,
+  nothing,
   property,
   ref
 } from '@toolbarthomas/enlightenment'
@@ -18,19 +19,50 @@ class EnlightenmentWindow extends Enlightenment {
   @property({ type: String })
   title?: string
 
-  render() {
+  @property({
+    reflect: true,
+    converter: (value) => Enlightenment.filterProperty(value, ['primary', 'secondary']),
+    type: String
+  })
+  type?: string = 'primary'
+
+  renderControls() {
     return html`
-      <div ref="${ref(this.context)}" class="window">
-        <header class="window__header">
-          <span class="window__meta">${this.title}</span>
-          <div class="window__controls">
-            <button class="window__control window__control--exit"></button>
-            <button class="window__control window__control--suspend"></button>
-            <button class="window__control window__control--toggle"></button>
-          </div>
-        </header>
+      <div class="window__controls">
+        <button class="window__control window__control--exit"></button>
+        <button class="window__control window__control--suspend"></button>
+        <button class="window__control window__control--toggle"></button>
+      </div>
+    `
+  }
+
+  renderHeader() {
+    if (this.type !== 'primary') {
+      return nothing
+    }
+
+    return html`
+      <header class="window__header">
+        <span class="window__meta">${this.title}</span>
+        ${this.renderControls()}
+      </header>
+    `
+  }
+
+  render() {
+    const classes = ['window']
+
+    if (this.type) {
+      classes.push(`window--type-is-${this.type}`)
+    }
+
+    return html`
+      <div ref="${ref(this.context)}" class="${classes.join(' ')}">
+        ${this.renderHeader()}
         <div class="window__body">
-          <aside class="window__aside">Body</aside>
+          <aside class="window__aside">
+            ${this.type === 'secondary' ? this.renderControls() : nothing}
+          </aside>
           <main class="window__content"><slot></slot></main>
         </div>
         <footer class="window__footer"></footer>
